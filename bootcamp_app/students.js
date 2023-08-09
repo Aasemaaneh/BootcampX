43a82da394117392b8bc39a39c65fa7c49f160b6
Aasemaaneh <aasemaaneh8@gmail.com>
2023-08-09 00:29:09 -0400

    SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,8 +1,5 @@
 const { Pool } = require('pg');
 
-//const pool = new Pool();
-
-
 const pool = new Pool({
   user: 'labber',
   password: '123',
@@ -10,17 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//Query
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-}).catch(err => console.error('query error', err.stack)); //.then(res => {}) gets executed, we're not dealing with SQL or the database any more, we're just dealing with JavaScript objects.The result is just an array of JavaScript objects.
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,3 @@
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -8,17 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+const cohortName = process.argv[2];
+
+const queryString = `
+  SELECT DISTINCT teachers.name
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name;
+`;
+
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
+  .then(res => {
+    console.log('connected');
+    res.rows.forEach(row => {
+      console.log(`${cohortName}: ${row.name}`);
+    });
   })
-});
+  .catch(err => console.error('query error', err.stack));
